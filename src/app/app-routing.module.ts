1b297067341ddda4e64546ee4f111213d41e1931
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-10-06 09:49:11 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { subscribeOn } from 'rxjs';
 import { SubscribeComponent } from './subscribe/subscribe.component';
+import { DailyquoteComponent } from './dailyquote/dailyquote.component';
+import { QoutesComponent } from './qoutes/qoutes.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {
-    redirectTo: "subscribe"
+    redirectTo: "dailyquote"
   },
   {
-    path: 'subscribe', 
-    component: SubscribeComponent
+    path: 'dailyquote', 
+    component: DailyquoteComponent
+  },
+  {
+    path: 'quotes', 
+    component: QoutesComponent
+  },
+  {
+    path: 'about', 
+    component: AboutComponent
   }
+  
+
 ];
 
 @NgModule({
