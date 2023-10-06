646fde27c584fd3ed0613dbbaff9b7ada539ac24
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-10-06 09:48:59 +0200

    Create dailyquote.component.ts

---
+++ src/app/dailyquote/dailyquote.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dailyquote',
+  templateUrl: './dailyquote.component.html',
+  styleUrls: ['./dailyquote.component.scss']
+})
+export class DailyquoteComponent {
+
+}
